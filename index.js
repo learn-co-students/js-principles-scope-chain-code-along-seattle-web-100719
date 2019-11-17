db125894ca3d1d94fc016ec1421be3f23f547165
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-16 21:12:18 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 /* Your code goes here */
+var customerName = "bob";
+
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+    bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer()
+{
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "what?";
+
+function changeLeastFavoriteCustomer()
+{
+    leastFavoriteCustomer = "who?";
+}
+
+function attemptTwoFavoriteCustomers()
+{
+   throw SyntaxError;
+}
